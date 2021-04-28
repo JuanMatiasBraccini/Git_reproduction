86466f10175fb6f339ee147b496f404c5b4e3314
Matias Braccini <Matias.Braccini@fish.wa.gov.au>
2021-04-28 13:38:36 +0800

    Changed to OneNote wd

--- Re fit whiskery shark maturity.R
+++ Re fit whiskery shark maturity.R
@@ -1,4 +1,5 @@
-handl_OneDrive=function(x)paste('C:/Users/myb/OneDrive - Department of Primary Industries and Regional Development/Matias',x,sep='/')
+if(!exists('handl_OneDrive')) source('C:/Users/myb/OneDrive - Department of Primary Industries and Regional Development/Matias/Analyses/SOURCE_SCRIPTS/Git_other/handl_OneDrive.R')
+
 Dat=read.csv(handl_OneDrive("Data/Life history parameters/Maturity_whisk_Simpfendorferetal1998.csv"))
 
 mod <- nls(Prop.Mat~1/(1+exp(-log(19)*(FL-p50)/(p95-p50))), start=c(p50=115, p95=120), data=Dat)
